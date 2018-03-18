0ec120c04dd199de03becdea82ae15a1a2a515d3
Luke Myers <lukemyers0921@gmail.com>
2018-03-18 11:48:05 -0400

    added moment.js added better styles

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,52 +1,58 @@
-
-        // Initialize Firebase
-        // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyBjCdmfhbGXP7USxCIuWhFUWEXvva0452k",
     authDomain: "train-scheduler-69e14.firebaseapp.com",
     databaseURL: "https://train-scheduler-69e14.firebaseio.com",
     projectId: "train-scheduler-69e14",
     storageBucket: "train-scheduler-69e14.appspot.com",
     messagingSenderId: "377039613518"
-  };
-  firebase.initializeApp(config);
-        // Create a variable to reference the database.
-        var database = firebase.database();
+};
+firebase.initializeApp(config);
+// Create a variable to reference the database.
+var database = firebase.database();
 
-        // Initial Values
-        var name = "";
-        var role = "";
-        var start = 0;
-        var rate = 0;
+// Initial Values
+var name = "";
+var role = "";
+var start = 0;
+var rate = 0;
 
 
 
-        // Capture Button Click
-        $("#add-train-btn").on("click", function (event) {
-            event.preventDefault();
-            console.log("hi")
-            // Grabbed values from text boxes
-            name = $("#name-input").val().trim();
-            destination = $("#destination-input").val().trim();
-            firstTrain = $("#first-train-input").val().trim();
-            frequency = $("#frequency-input").val().trim();
-            // Code for handling the push
-            database.ref().push({
-                name: name,
-                destination: destination,
-                firstTrain: firstTrain,
-                frequency: frequency,
-                dateAdded: firebase.database.ServerValue.TIMESTAMP
-            });
+// Capture Button Click
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+    console.log("hi")
+    // Grabbed values from text boxes
+    name = $("#name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    firstTrain = $("#first-train-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+    // Code for handling the push
+    database.ref().push({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
 
-        });
-        database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
-            var data = snapshot.val();
-            
-            var nextArrival = ""
-            var minutesAway = "";
-            var tableRow =
-            `
+});
+database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
+    var data = snapshot.val();
+    var time = moment();
+    var tFrequency = data.frequency
+    var firstTime = data.firstTrain
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    moment(currentTime).format("hh:mm");
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % tFrequency;
+    var minutesAway = tFrequency - tRemainder;
+    var nextTrain = moment().add(minutesAway    , "minutes");
+    var nextArrival = moment(nextTrain).format("hh:mm");
+    var tableRow =
+        `
             <tr>
                 <th scope="row">${data.name}</th>
                 <td>${data.destination}</td>
@@ -55,8 +61,8 @@
                 <td>${minutesAway}</td>
             </tr>
             `
-            $('tbody').append(tableRow);
-        }, function (errorObject) {
-            console.log("Errors handled: " + errorObject.code);
+    $('tbody').append(tableRow);
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
 
-        });
+});
